d956298f57ea5168ee9909502a21551c03224c0e
bakhtiarabid <abid.bu16@gmail.com>
2021-10-04 10:33:40 +0600

    Top-Banner Section Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./Components/Header/Header";
@@ -8,35 +7,40 @@ import Services from "./Components/Services/Services";
 import About from "./Components/About/About";
 import Cart from "./Components/Cart/Cart";
 import NotFound from "./Components/NotFound/NotFound";
+import { createContext } from "react";
+
+export const ServiceContext = createContext("services");
 
 function App() {
    return (
-      <div className="App">
-         <Router>
-            <Header></Header>
-            <Switch>
-               <Route exact path="/home">
-                  <Home></Home>
-               </Route>
-               <Route exact path="/about">
-                  <About></About>
-               </Route>
-               <Route exact path="/services">
-                  <Services></Services>
-               </Route>
-               <Route exact path="/cart">
-                  <Cart></Cart>
-               </Route>
-               <Route exact path="/">
-                  <Home></Home>
-               </Route>
-               <Route path="*">
-                  <NotFound></NotFound>
-               </Route>
-            </Switch>
-            <Footer></Footer>
-         </Router>
-      </div>
+      <ServiceContext.Provider>
+         <div>
+            <Router>
+               <Header></Header>
+               <Switch>
+                  <Route exact path="/home">
+                     <Home></Home>
+                  </Route>
+                  <Route exact path="/services">
+                     <Services></Services>
+                  </Route>
+                  <Route exact path="/about">
+                     <About></About>
+                  </Route>
+                  <Route exact path="/cart">
+                     <Cart></Cart>
+                  </Route>
+                  <Route exact path="/">
+                     <Home></Home>
+                  </Route>
+                  <Route path="*">
+                     <NotFound></NotFound>
+                  </Route>
+               </Switch>
+               <Footer></Footer>
+            </Router>
+         </div>
+      </ServiceContext.Provider>
    );
 }
 
--- src/Components/TopItems/TopItems.js
+++ src/Components/TopItems/TopItems.js
@@ -1,11 +1,32 @@
 import React from "react";
+import "./TopItems.css";
 
 const TopItems = () => {
    return (
       <div>
-         <h1>This is topItems</h1>
+         <div className="hero-image">
+            <div className="hero-text">
+               <h1 style={{ fontSize: "50px" }}>
+                  Welcome To Pyramid Architecture Online Courses
+               </h1>
+               <p>
+                  Explore these architectural lessons to see what tickles your
+                  interest.
+               </p>
+               <button>Get Started</button>
+            </div>
+         </div>
       </div>
    );
 };
 
 export default TopItems;
+
+/* {
+   /* <div className="hero-image">
+            <div className="hero-text">
+               <h1 style={{ fontSize: "50px" }}>I am John Doe</h1>
+               <p>And I'm a Photographer</p>
+               <button>Hire me</button>
+            </div>
+         </div> */
